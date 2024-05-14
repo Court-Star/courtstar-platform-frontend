f83708f770a680a3567868031e89b2cec5640bea
bd_hoag <hoangakiy@gmail.com>
2024-05-14 16:54:24 +0700

    test: push code to repo

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-gray-400">
+        <h1 className="text-gray-400">
           Chuẩn bị tinh thần build UI nhé ae
-        </p>
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
